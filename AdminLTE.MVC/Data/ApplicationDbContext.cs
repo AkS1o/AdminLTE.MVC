5c4d763226c919ad271fbd258fa01e92ce489e80
Віталік <vitalik.baziuk228@gmail.com>
2021-07-29 18:12:03 +0300

    Add Community

--- AdminLTE.MVC/Data/ApplicationDbContext.cs
+++ AdminLTE.MVC/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using AdminLTE.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,7 @@ namespace AdminLTE.MVC.Data
             : base(options)
         {
         }
+
+        public DbSet<Community> Communities { get; set; }
     }
 }
---
+++ AdminLTE.MVC/Migrations/20210729150523_CreateCommunity.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdminLTE.Migrations
+{
+    public partial class CreateCommunity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Communities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Region = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CommunityType = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Communities", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Communities");
+        }
+    }
+}
